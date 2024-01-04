83c46199643aa866a897a22985f249a70ecd03ae
deveno <dias_2003_2010@mail.ru>
2024-01-04 22:35:22 +0600

    updated about function views.py

--- main/views.py
+++ main/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница сайта',
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
     context = {
-        'title': 'About',
-        'content': 'Страница о нас!'
+        'title': 'Home - О нас',
+        'content': 'О нас!',
+        'text_on_page': 'Какое-то описание магазина - бла, бла, бла!'
     }
     return render(request, 'main/about.html', context)

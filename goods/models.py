8783703e9edec865e44f67df3badc5030f690430
deveno <dias_2003_2010@mail.ru>
2024-01-07 01:22:49 +0600

    added model Category

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,6 @@
+from enum import unique
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)

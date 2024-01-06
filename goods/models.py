fb612a27a5ce4fec8a751b0981aef7180553577c
deveno <dias_2003_2010@mail.ru>
2024-01-07 01:46:49 +0600

    added and updated model category to admin panel

--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,11 @@
 from enum import unique
+from tabnanny import verbose
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
